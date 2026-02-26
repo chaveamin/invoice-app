25037dc3f9550b7f2d5c03aff861687c5fb32b37
Amin <aminchave@gmail.com>
2026-02-26 11:40:14 +0330

    nextjs.16

--- utils/pdf-generator.tsx
+++ utils/pdf-generator.tsx
@@ -3,27 +3,15 @@ import { jsPDF } from "jspdf";
 import "@/utils/YekanBakhFaNum-Bold-bold";
 
 export const generatePDF = (invoice: InvoiceData) => {
-  const doc = new jsPDF();
-  const startY = 10;
-  let y = startY;
+  const doc = new jsPDF("p", "mm", [210, 390]);
+  let y = 10;
 
   doc.setLanguage("fa-IR");
   doc.setFont("YekanBakhFaNum-Bold", "normal", "bold");
 
-  let calculatedHeight = 63;
-
-  invoice.items.forEach((item) => {
-    const descriptionLines = doc.splitTextToSize(item.desc, 85);
-
-    const textHeight = descriptionLines.length * 5;
-
-    const rowHeight = Math.max(textHeight, 10) + 15;
-    calculatedHeight += rowHeight;
-  });
-
   doc.setDrawColor(229, 231, 235);
   doc.setFillColor(255, 255, 255);
-  doc.roundedRect(10, y, 190, calculatedHeight, 6, 6, "FD");
+  doc.roundedRect(10, y, 190, 370, 6, 6, "FD");
   y += 10;
 
   doc.setDrawColor(255, 255, 255);
@@ -48,33 +36,24 @@ export const generatePDF = (invoice: InvoiceData) => {
   doc.setFontSize(12);
   doc.setTextColor(107, 114, 128);
   doc.text("توضیحات", 170, y);
-  doc.text("تعداد", 140, y);
-  doc.text("قیمت واحد", 100, y);
+  doc.text("تعداد", 120, y);
+  doc.text("قیمت واحد", 70, y);
   doc.text("قیمت", 25, y);
   y += 13;
 
   invoice.items.forEach((item) => {
-    const startOfRowY = y;
     doc.setTextColor(17, 24, 39);
 
-    const descriptionLines = doc.splitTextToSize(item.desc, 85);
-    const textHeight = descriptionLines.length * 5;
-    const rowHeight = Math.max(textHeight, 1);
+    doc.text(item.desc, 187, y, { align: "right" });
+    doc.text(item.quantity.toString(), 125, y);
+    doc.text(`${Number(item.rate).toLocaleString("fa-IR")}`, 70, y);
+    doc.text(`${item.amount.toLocaleString("fa-IR")}`, 25, y);
 
-    doc.text(descriptionLines, 187, startOfRowY, { align: "right" });
-    doc.text(item.quantity.toString(), 145, startOfRowY);
-    doc.text(`${Number(item.rate).toLocaleString("fa-IR")}`, 100, startOfRowY);
-    doc.text(`${item.amount.toLocaleString("fa-IR")}`, 25, startOfRowY);
-
-    y += rowHeight;
-
-    y += 3;
+    y += 7;
     doc.setDrawColor(229, 231, 235);
     doc.line(20, y, 190, y);
     y += 7;
   });
-
-  // Totals
 
   doc.setDrawColor(255, 255, 255);
   doc.setFillColor(37, 99, 235);
@@ -86,6 +65,25 @@ export const generatePDF = (invoice: InvoiceData) => {
   doc.text(":مبلغ نهایی", 163, y);
   doc.text(`${invoice.total.toLocaleString("fa-IR")}`, 37, y);
   doc.text("تومان", 23, y);
+
+  y += 12;
+  doc.setFontSize(10);
+  doc.setLineWidth(0.35);
+  doc.setDrawColor(37, 99, 235);
+  doc.setFillColor(255, 255, 255);
+  doc.roundedRect(20, y, 170, 50, 3, 3, "FD");
+  doc.setTextColor(107, 114, 128);
+  y += 10;
+  doc.text("قبل از شروع کار 50 درصدر از کل مبلغ فاکتور پرداخت میشود", 185, y, { align: "right" });
+  y += 10;
+  doc.text("جهت مشاهده پیشرفت پروژه; کار در ورسل آپلود میشود", 185, y, { align: "right" });
+  y += 10;
+  doc.setLineHeightFactor(2);
+  const textContent =
+    "اولین ویرایش رایگان است: بعد از تحویل; کار توسط کارفرما بررسی میشود و در صورت هر گونه اصلاحات همه آنها را در یک پیام به طراح گزارش میدهد. بعد از اصلاح موارد هرگونه ویرایشات بعدی با هزینه انجام میشود";
+  const maxWidth = 190;
+  const splitText = doc.splitTextToSize(textContent, maxWidth);
+  doc.text(splitText, 185, y, { align: "right" });
 
   // Generate blob URL untuk preview
   const pdfBlob = doc.output("blob");
